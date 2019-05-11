e59337f990b248030956604d0a2bbd4b20cf703c
Ed Maphis <emaphis85@gmail.com>
2019-05-10 20:20:35 -0400

    Some tasks.

---
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task03/task0302/Solution.java
@@ -0,0 +1,16 @@
+package com.codegym.task.task03.task0302;
+
+/* 
+Display right away
+
+*/
+public class Solution {
+    // write your code here
+    public static void printString(String str) {
+        System.out.println(str);
+    }
+
+    public static void main(String[] args) {
+        printString("Hello, Amigo!");
+    }
+}
