aa146d355db2202a714411393753d073030b8506
Ed Maphis <emaphis85@gmail.com>
2019-05-21 14:42:18 -0400

    Start level 8 HashMap<T,U>, Set<T,U>

---
+++ CodeGymTasks/1.JavaSyntax/src/com/codegym/task/task08/task0803/Solution.java
@@ -0,0 +1,47 @@
+package com.codegym.task.task08.task0803;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+HashMap of cats
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        String[] cats = new String[]{"Tiger", "Missy", "Smokey", "Marmalade", "Oscar", "Snowball", "Boss", "Smudge", "Max", "Simba"};
+
+        HashMap<String, Cat> map = addCatsToMap(cats);
+
+        for (Map.Entry<String, Cat> pair : map.entrySet()) {
+            System.out.println(pair.getKey() + " - " + pair.getValue());
+        }
+    }
+
+
+    public static HashMap<String, Cat> addCatsToMap(String[] cats) {
+        //write your code here
+        HashMap<String, Cat> catMap = new HashMap<String, Cat>();
+        for (int i = 0; i < cats.length; i++) {
+            String name = cats[i];
+            catMap.put(name, new Cat(name));
+        }
+
+        return catMap;
+    }
+
+
+    public static class Cat {
+        String name;
+
+        public Cat(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name != null ? name.toUpperCase() : null;
+        }
+    }
+}
