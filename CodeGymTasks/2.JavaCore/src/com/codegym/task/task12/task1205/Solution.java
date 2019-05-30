4ca5299123d057f5c62fd736fba0dbbad133ec06
Ed Maphis <emaphis85@gmail.com>
2019-05-30 18:19:08 -0400

    Start on level 12. Simple polymorphism.

---
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task12/task1205/Solution.java
@@ -0,0 +1,43 @@
+package com.codegym.task.task12.task1205;
+
+/* 
+Animal identification
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getObjectType(new Cow()));
+        System.out.println(getObjectType(new Dog()));
+        System.out.println(getObjectType(new Whale()));
+        System.out.println(getObjectType(new Pig()));
+    }
+
+    public static String getObjectType(Object o) {
+        //write your code here
+        String type;
+        if (o instanceof Cow) {
+            type = "Cow";
+        } else if (o instanceof Dog) {
+            type = "Dog";
+        } else if (o instanceof Whale) {
+            type = "Whale";
+        } else {
+            type = "Unknown Animal";
+        }
+
+        return type;
+    }
+
+    public static class Cow {
+    }
+
+    public static class Dog {
+    }
+
+    public static class Whale {
+    }
+
+    public static class Pig {
+    }
+}
