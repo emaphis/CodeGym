b2b1593baadf98303f267321a721353592e33551
Ed Maphis <emaphis85@gmail.com>
2019-05-31 21:09:54 -0400

    Finish level 12 Exercises

---
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task12/task1229/Solution.java
@@ -0,0 +1,29 @@
+package com.codegym.task.task12.task1229;
+
+/* 
+Parent of the CTO class
+
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        CTO cto = new CTO();
+        System.out.println(cto);
+    }
+
+    public static interface Businessman {
+        public void workHard();
+    }
+
+    public static class Parent implements Businessman {
+
+        @Override
+        public void workHard() {
+        }
+    }
+
+    public static class CTO extends Parent implements Businessman {
+
+    }
+}
