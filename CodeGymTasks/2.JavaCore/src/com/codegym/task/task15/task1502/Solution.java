cb3976fe82c6d5ee071903f1aadb8c61d80a5dc5
Ed Maphis <emaphis85@gmail.com>
2019-06-05 18:16:21 -0400

    Level 15 Object inheritance.

---
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task15/task1502/Solution.java
@@ -0,0 +1,47 @@
+package com.codegym.task.task15.task1502;
+
+/* 
+OOP: Animal inheritance
+
+*/
+
+public class Solution {
+    // Add the Goose and Dragon classes here
+
+    public static void main(String[] args) {
+        SmallAnimal small = new Goose();
+        System.out.println(small.getSize());
+        BigAnimal big = new Dragon();
+        System.out.println(big.getSize());
+    }
+
+    public static class BigAnimal {
+        protected String getSize() {
+            return "like a dinosaur";
+        }
+    }
+
+    public static class SmallAnimal {
+        String getSize() {
+            return "like a cat";
+        }
+    }
+
+    public static class Goose extends SmallAnimal {
+
+        @Override
+        public String getSize() {
+            return "A goose is small, " + super.getSize();
+        }
+
+    }
+
+    public static class Dragon extends BigAnimal {
+
+        @Override
+        public String getSize() {
+            return "A dragon is big, " + super.getSize();
+        }
+    }
+
+}
