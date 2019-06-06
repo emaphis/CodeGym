87a0c122875bf0d7751031e879f8c17aa7ad35fb
Ed Maphis <emaphis85@gmail.com>
2019-06-05 23:03:00 -0400

    Practice overloading methods

---
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task15/task1507/Solution.java
@@ -0,0 +1,67 @@
+package com.codegym.task.task15.task1507;
+
+/* 
+OOP: Method overloading
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        printMatrix(2, 3, "8");
+    }
+
+    public static void printMatrix(int m, int n, String value) {
+        System.out.println("Filling with String objects");
+        printMatrix(m, n, (Object) value);
+    }
+
+    public static void printMatrix(int m, int n, Object value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void printMatrix(int m, int n, int value) {
+        System.out.println("Filling with String objects");
+        printMatrix(m, n, (Object) value);
+    }
+
+    public static void printMatrix(int m, int n, long value) {
+        System.out.println("Filling with String objects");
+        printMatrix(m, n, (Object) value);
+    }
+
+    public static void printMatrix(int m, int n, short value) {
+        System.out.println("Filling with String objects");
+        printMatrix(m, n, (Object) value);
+    }
+
+    public static void printMatrix(int m, int n, Integer value) {
+        System.out.println("Filling with String objects");
+        printMatrix(m, n, (Object) value);
+    }
+
+    public static void printMatrix(int m, int n, double value) {
+        System.out.println("Filling with String objects");
+        printMatrix(m, n, (Object) value);
+    }
+
+    public static void printMatrix(int m, int n, float value) {
+        System.out.println("Filling with String objects");
+        printMatrix(m, n, (Object) value);
+    }
+
+    public static void printMatrix(short m, short n, String value) {
+        System.out.println("Filling with String objects");
+        printMatrix(m, n, (Object) value);
+    }
+
+    public static void printMatrix(byte m, byte n, String value) {
+        System.out.println("Filling with String objects");
+        printMatrix(m, n, (Object) value);
+    }
+}
