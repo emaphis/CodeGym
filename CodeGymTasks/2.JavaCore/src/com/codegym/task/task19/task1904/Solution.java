a79239553e331d5805cf97936bcfbe0e1245b346
Ed Maphis <emaphis85@gmail.com>
2019-06-27 17:11:39 -0400

    More on Adaptor Pattern and FileInputStream, FileOutputStream.

--- CodeGymTasks/2.JavaCore/src/com/codegym/task/task19/task1904/Solution.java
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task19/task1904/Solution.java
@@ -1,9 +1,10 @@
 package com.codegym.task.task19.task1904;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Scanner;
+import java.util.*;
 
 /* 
 Yet another adapter
@@ -13,7 +14,18 @@ Yet another adapter
 public class Solution {
 
     public static void main(String[] args) {
-
+        List<Person> list = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(new FileReader("persons.txt"));
+            PersonScanner personScanner = new PersonScannerAdapter(scanner);
+            Person person1 = personScanner.read();
+            Person person2 = personScanner.read();
+            personScanner.close();
+            System.out.println(person1);
+            System.out.println(person2);
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
     }
 
     public static class PersonScannerAdapter implements PersonScanner {
@@ -25,14 +37,22 @@ public class Solution {
 
         @Override
         public Person read() throws IOException {
+            Person person = null;
             String line = fileScanner.nextLine();
             String[] data = line.split(" ");
-            String lastName = data[0];
-            String firstName = data[1];
-            String middleName = data[2];
-            String strDate =  data[3] + " " + data[4] + " " + data[5];
-            Date date = new SimpleDateFormat("d M y").parse(strDate);
-            return new Person(firstName, middleName, lastName, date);
+
+            String firstName = data[0];
+            String middleName = data[1];
+            String lastName = data[2];
+            int month = Integer.parseInt(data[3]) - 1;
+            int day = Integer.parseInt(data[4]);
+            int year = Integer.parseInt(data[5]);
+
+            Date date = new GregorianCalendar(year, month, day).getTime();
+
+            person = new Person(lastName, firstName, middleName, date);
+
+            return person;
         }
 
         @Override
---
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task19/task1905/Solution.java
@@ -0,0 +1,116 @@
+package com.codegym.task.task19.task1905;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Reinforce the adapter
+
+*/
+
+public class Solution {
+    public static Map<String,String> countries = new HashMap<>();
+
+    static {
+        countries.put("UA", "Ukraine");
+        countries.put("US", "United States");
+        countries.put("FR", "France");
+    }
+
+    public static void main(String[] args) {
+        Customer customer = new Customer() {
+            @Override
+            public String getCompanyName() {
+                return "CodeGym Ltd";
+            }
+
+            @Override
+            public String getCountryName() {
+                return "United States";
+            }
+        };
+
+        Contact contact = new Contact() {
+            @Override
+            public String getName() {
+                return "Peterson, John";
+            }
+
+            @Override
+            public String getPhoneNumber() {
+                return "+1(111)222-3333";
+            }
+        };
+
+        DataAdapter rowItem = new DataAdapter(customer, contact);
+        System.out.println(rowItem.getCompany());
+        System.out.println(rowItem.getContactFirstName());
+        System.out.println(rowItem.getContactLastName());
+        System.out.println(rowItem.getCountryCode());
+        System.out.println(rowItem.getDialString());
+    }
+
+    public static class DataAdapter implements RowItem {
+        private Customer customer;
+        private Contact contact;
+
+        public DataAdapter(Customer customer, Contact contact) {
+            this.customer = customer;
+            this.contact = contact;
+        }
+
+        @Override
+        public String getCountryCode() {
+            String code = null;
+            String countryName = customer.getCountryName();
+            // search for key by value
+            for (Map.Entry<String, String> entry : countries.entrySet()) {
+                if (entry.getValue().equals(countryName)) {
+                    code = entry.getKey();
+                }
+            }
+            return code;
+        }
+
+        @Override
+        public String getCompany() {
+            return customer.getCompanyName();
+        }
+
+        @Override
+        public String getContactFirstName() {
+            String[] names = contact.getName().split(", ");
+            return names[1];
+        }
+
+        @Override
+        public String getContactLastName() {
+            String[] names = contact.getName().split(", ");
+            return names[0];
+        }
+
+        @Override
+        public String getDialString() {
+            String dialURL = "callto://" + contact.getPhoneNumber().replaceAll("[()-]", "");
+            return dialURL;
+        }
+    }
+
+    public static interface RowItem {
+        String getCountryCode();        // For example: US
+        String getCompany();            // For example: CodeGym Ltd.
+        String getContactFirstName();   // For example: John
+        String getContactLastName();    // For example: Peterson
+        String getDialString();         // For example: callto://+11112223333
+    }
+
+    public static interface Customer {
+        String getCompanyName();        // For example: CodeGym Ltd.
+        String getCountryName();        // For example: United States
+    }
+
+    public static interface Contact {
+        String getName();               // For example: Peterson, John
+        String getPhoneNumber();        // For example: +1(111)222-3333, +3(805)0123-4567, +380(50)123-4567, etc.
+    }
+}
